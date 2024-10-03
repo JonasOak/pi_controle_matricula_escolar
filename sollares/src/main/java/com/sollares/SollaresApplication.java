72a61e8c5e26a409efce320334a259ac50dcfb64
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-10-03 18:29:23 -0300

    classe executável

---
+++ sollares/src/main/java/com/sollares/SollaresApplication.java
@@ -0,0 +1,13 @@
+package com.sollares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SollaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SollaresApplication.class, args);
+	}
+
+}
