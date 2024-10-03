ba78f6afd4c948fba1441b35f3e13c932ae8d394
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-10-03 18:34:13 -0300

    services Pessoa

---
+++ sollares/src/main/java/com/sollares/services/PessoaService.java
@@ -0,0 +1,50 @@
+package com.sollares.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sollares.entities.Pessoa;
+import com.sollares.repositories.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository repository;
+	
+	public List<Pessoa> buscarTodos() {
+		return repository.findAll();
+	}
+	
+	public Pessoa buscarPorId(Integer id) {
+		Optional<Pessoa> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	public Pessoa inserir(Pessoa obj) {
+		return repository.save(obj);
+	}
+	
+	public void deletar(Integer id) {
+		repository.deleteById(id);
+	}
+	
+	public Pessoa atualizar(Integer id, Pessoa obj) {
+		Pessoa entity = repository.getReferenceById(id);
+		atualizarDados(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void atualizarDados(Pessoa entity, Pessoa obj) {
+		entity.setNomePessoa(obj.getNomePessoa());
+		entity.setEndereco(obj.getEndereco());
+		entity.setUf(obj.getUf());
+		entity.setTelefone(obj.getTelefone());
+		entity.setCpf(obj.getCpf());
+		entity.setEmail(obj.getEmail());
+	}
+}
+	
