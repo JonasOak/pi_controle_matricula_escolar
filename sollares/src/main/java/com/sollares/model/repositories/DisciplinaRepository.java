2c2ffd1567cd4c93b74b3af9ce1724dd13d21610
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-10-14 18:23:12 -0300

    método de buscar por disciplina

--- sollares/src/main/java/com/sollares/model/repositories/DisciplinaRepository.java
+++ sollares/src/main/java/com/sollares/model/repositories/DisciplinaRepository.java
@@ -1,9 +1,14 @@
 package com.sollares.model.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sollares.model.entities.Disciplina;
+import com.sollares.model.entities.Matricula;
 
 public interface DisciplinaRepository extends JpaRepository<Disciplina, Integer> {
+	
+	List<Matricula> findByDisciplina(Disciplina disciplina);
 
 }
