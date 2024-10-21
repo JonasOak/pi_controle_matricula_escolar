91c4f886b2ecb0eb9f2857582308fdb980c9a7bc
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-10-21 09:54:08 -0300

    entidade usuário

---
+++ sollares/src/main/java/com/sollares/model/entities/Usuario.java
@@ -0,0 +1,98 @@
+package com.sollares.model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idUsuario;
+	private String nome;
+	private String cargo;
+	private String login;
+	private String senha;
+	private String email;
+	
+	public Usuario() {
+	}
+
+	public Usuario(int idUsuario, String nome, String cargo, String login, String senha, String email) {
+		this.idUsuario = idUsuario;
+		this.nome = nome;
+		this.cargo = cargo;
+		this.login = login;
+		this.senha = senha;
+		this.email = email;
+	}
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idUsuario);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return idUsuario == other.idUsuario;
+	}
+}
