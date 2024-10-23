6c30dd6bd8611e966b167c13770b75486137d030
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-10-23 17:41:13 -0300

    nome dado às colunas da tabela pessoa

--- sollares/src/main/java/com/sollares/model/entities/Pessoa.java
+++ sollares/src/main/java/com/sollares/model/entities/Pessoa.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,11 +23,23 @@ public class Pessoa implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idPessoa;
+	
+	@Column(name = "nome_pessoa")
 	private String nomePessoa;
+	
+	@Column(name = "endereco")
 	private String endereco;
+	
+	@Column(name = "uf")
 	private String uf;
+	
+	@Column(name = "telefone")
 	private String telefone;
+	
+	@Column(name = "cpf")
 	private String cpf;
+	
+	@Column(name = "email")
 	private String email;
 	
 	@JsonIgnore
