44dc16b0e24f5993baefa3b6215f76ada07cbe0c
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-10-23 17:38:02 -0300

    acesso à lista de alunos e professores

--- sollares/src/main/java/com/sollares/controller/resources/PessoaResources.java
+++ sollares/src/main/java/com/sollares/controller/resources/PessoaResources.java
@@ -5,25 +5,42 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sollares.controller.services.PessoaService;
 import com.sollares.model.entities.Pessoa;
+import com.sollares.model.repositories.PessoaRepository;
 
-@RestController
-@RequestMapping(value = "/pessoas")
+@Controller
 public class PessoaResources {
 
 	@Autowired
 	private PessoaService servico;
+	
+	@Autowired
+    private PessoaRepository pessoaRepository;
+	
+	@GetMapping("/professores")
+	public String getVisualizarProfessores(Model model) {
+		List<Pessoa> listaProfessores = pessoaRepository.findDistinctProfessores();
+		model.addAttribute("listaProfessores", listaProfessores);
+		return "listTodosProf";
+	}
+		
+	@GetMapping("/alunos")
+	public String getVisualizarAlunos(Model model) {
+		List<Pessoa> listaAlunos = pessoaRepository.findDistinctAlunos();
+		model.addAttribute("listaAlunos", listaAlunos);
+		return "listTodosAlun";
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Pessoa>> buscarTodos() {
