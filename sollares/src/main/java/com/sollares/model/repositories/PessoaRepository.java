51fbf9e2fd1c5f4b08fa32af483369788d5a0091
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-10-23 17:42:09 -0300

    implementação de query com lista de alunos e professores

--- sollares/src/main/java/com/sollares/model/repositories/PessoaRepository.java
+++ sollares/src/main/java/com/sollares/model/repositories/PessoaRepository.java
@@ -1,9 +1,18 @@
 package com.sollares.model.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sollares.model.entities.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
-
+	
+	@Query("SELECT p FROM Pessoa p JOIN Disciplina d ON p.idPessoa = d.professor.idPessoa")
+	List<Pessoa> findDistinctProfessores();
+	
+	@Query("SELECT p FROM Pessoa p JOIN Matricula d ON p.idPessoa = d.aluno.idPessoa")
+	List<Pessoa> findDistinctAlunos();
+	
 }
