72263f9e1477adbb85c83dd69a8089904e049eed
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-10-29 12:38:43 -0300

    implementação do atualizar, deletar, buscar e melhoria do cadastrar

--- sollares/src/main/java/com/sollares/controller/resources/PessoaResources.java
+++ sollares/src/main/java/com/sollares/controller/resources/PessoaResources.java
@@ -3,16 +3,15 @@ package com.sollares.controller.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sollares.controller.services.PessoaService;
 import com.sollares.model.entities.Pessoa;
@@ -27,15 +26,15 @@ public class PessoaResources {
 	@Autowired
     private PessoaRepository pessoaRepository;
 	
-	@GetMapping("/")
-	public String getVisualizarPortal(Model model) {
+	@GetMapping("/index")
+	public String getVisualizarPortal() {
 	    return "portal";
 	}
 	
-	@GetMapping("/manterPessoa")
+	@GetMapping("/cadastrarPessoa")
 	public String getCrudPessoa(Model model) {
 		model.addAttribute("pessoa", new Pessoa());
-		return "pessoa";
+		return "cadastrarPessoa";
 	}
 	
 	@GetMapping("/pessoas")
@@ -43,45 +42,78 @@ public class PessoaResources {
 		List<Pessoa> listaPessoas = servico.buscarTodos();
 		model.addAttribute("listaPessoas", listaPessoas);
 		model.addAttribute("pessoa", new Pessoa());
-		return "listTodasPessoas";
+		return "listarPessoas";
 	}
 	
 	@GetMapping("/professores")
 	public String getVisualizarProfessores(Model model) {
 		List<Pessoa> listaProfessores = pessoaRepository.findDistinctProfessores();
 		model.addAttribute("listaProfessores", listaProfessores);
-		return "listTodosProf";
+		return "listarProfessores";
 	}
 		
 	@GetMapping("/alunos")
 	public String getVisualizarAlunos(Model model) {
 		List<Pessoa> listaAlunos = pessoaRepository.findDistinctAlunos();
 		model.addAttribute("listaAlunos", listaAlunos);
-		return "listTodosAlun";
+		return "listarAlunos";
 	}
 	
-	@GetMapping("/pessoa/{id}")
-    public String buscarPessoa(@PathVariable(value = "id") long id,Model model) {
-        Pessoa pessoa = servico.buscarPorId((int) id);
-        return "pessoaProfessor";
+	@GetMapping("/pessoa/buscar")
+    public String buscarPessoaPorNome(@RequestParam(value = "nome", required = false) String nome, Model model) {
+		List<Pessoa> listaPessoas = servico.buscarPorNome(nome);
+	    model.addAttribute("listaPessoas", listaPessoas);
+	    return "listarPessoas";
     }
 	
     @PostMapping("/pessoa")
-    public String inserir(@ModelAttribute("pessoa") Pessoa pessoa) {
-        servico.inserir(pessoa);
+    public String inserir(@ModelAttribute("pessoa") Pessoa pessoa, RedirectAttributes redirectAttributes) {
+    	try {
+    		servico.inserir(pessoa);
+    	} catch (Exception e) {
+    		redirectAttributes.addFlashAttribute("ErroInserir", "Algum erro impediu de cadastrar uma pessoa.");
+            return "redirect:/pessoas";
+    	}
+    	redirectAttributes.addFlashAttribute("PessoaCadastrada", "Pessoa cadastrada com sucesso.");
+        return "redirect:/pessoas";
+    }
+    
+    @PostMapping("/confirmarAtualizar/{id}")
+    public String atualizar(@PathVariable("id") int id, @ModelAttribute("pessoa") Pessoa pessoa, RedirectAttributes redirectAttributes) {
+        try {
+            pessoa.setIdPessoa(id); 
+            servico.atualizar(pessoa);
+            redirectAttributes.addFlashAttribute("PessoaAtualizada", "Pessoa atualizada com sucesso.");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("ErroAtualizar", "Algum erro impediu a atualização da pessoa.");
+            return "redirect:/pessoas";
+        }
+        return "redirect:/pessoas";
+    }
+    
+    @GetMapping("/confirmarDeletar/{id}")
+    public String deletar(@PathVariable(value = "id") int id, RedirectAttributes redirectAttributes){
+    	try {
+            servico.deletar(id);
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("ErroDeletar", "Não é possível deletar essa pessoa pois ela está associada como aluno ou professor.");
+            return "redirect:/pessoas";
+        }
+    	redirectAttributes.addFlashAttribute("PessoaDeletada", "Pessoa deletada com sucesso.");
         return "redirect:/pessoas";
     }
 	
-	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> deletar(@PathVariable Integer id) {
-		servico.deletar(id);
-		return ResponseEntity.noContent().build();
+	@GetMapping("/deletarPessoa/{id}")
+	public String mostrarDeletrarPessoa(@PathVariable(value = "id") int id, Model model) {
+		Pessoa pessoa = servico.buscarPorId(id);
+		model.addAttribute("pessoa", pessoa);
+		return "deletarPessoa";
 	}
 	
-	@PutMapping(value = "/{id}")
-	public ResponseEntity<Pessoa> atualizar(@PathVariable Integer id, @RequestBody Pessoa obj) {
-		obj = servico.atualizar(id, obj);
-		return ResponseEntity.ok().body(obj);
-	}
-		
+	@GetMapping("/atualizarPessoa/{id}")
+	public String mostrarAtualizarPessoa(@PathVariable(value = "id") int id, Model model) {
+		Pessoa pessoa = servico.buscarPorId(id);
+		model.addAttribute("pessoa", pessoa);
+		return "atualizarPessoa";
+	}	
 }
