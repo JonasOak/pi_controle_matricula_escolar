7b6c44f1eb1ea8a2c46df808a67809feaeab180c
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-10-29 12:39:36 -0300

    consulta para buscar por nome

--- sollares/src/main/java/com/sollares/model/repositories/PessoaRepository.java
+++ sollares/src/main/java/com/sollares/model/repositories/PessoaRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sollares.model.entities.Pessoa;
 
@@ -15,4 +16,6 @@ public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 	@Query("SELECT p FROM Pessoa p JOIN Matricula d ON p.idPessoa = d.aluno.idPessoa")
 	List<Pessoa> findDistinctAlunos();
 	
+	@Query("SELECT p FROM Pessoa p WHERE LOWER(p.nomePessoa) LIKE LOWER(CONCAT('%', :nome, '%'))")
+    List<Pessoa> buscarNomes(@Param("nome") String nome);
 }
