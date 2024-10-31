538c25a2ec67d14650fff716ec553c72ac77aee3
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-10-31 13:32:13 -0300

    classe de exceção personalizada para serviços

---
+++ sollares/src/main/java/com/sollares/exception/ExcecaoServico.java
@@ -0,0 +1,9 @@
+package com.sollares.exception;
+
+public class ExcecaoServico extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public ExcecaoServico(String message) {
+		super(message);
+	}
+}
